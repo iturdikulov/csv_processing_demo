1726019ca479d6e1dd11e45b5d9948efbc90a57d
Inom T. <i@inomoz.ru>
2025-12-10 19:15:08 +0300

    Fix "main" import in package & script

--- src/csv_processing/__init__.py
+++ src/csv_processing/__init__.py
@@ -1,4 +1,5 @@
 from csv_processing.csv_loader import CSVLoader
 from csv_processing.report_performance import PerformanceReport
+from csv_processing.main import main
 
-__all__ = ["CSVLoader", "PerformanceReport"]
+__all__ = ["CSVLoader", "PerformanceReport", "main"]
