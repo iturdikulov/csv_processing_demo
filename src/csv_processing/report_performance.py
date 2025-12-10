0f5a9f850e61ce123d31c78dfe8dfc7d1aac391b
Inom T. <i@inomoz.ru>
2025-12-10 17:02:40 +0300

    performance_report: adjust typing

--- src/csv_processing/report_performance.py
+++ src/csv_processing/report_performance.py
@@ -2,17 +2,19 @@ import logging
 
 from statistics import mean
 from collections import defaultdict
-from typing import Generator
+from collections.abc import Generator
+from typing import final
 
 logger = logging.getLogger(__name__)  # default level is warning
 
 
+@final
 class PerformanceReport:
     """
     A class to generate a performance report from a data generator.
     """
 
-    def __init__(self, data_generator: Generator):
+    def __init__(self, data_generator: Generator[dict[str, str], None, None]):
         """
         Initialize the PerformanceReport with a data generator and
         report-specific headers.
@@ -20,11 +22,11 @@ class PerformanceReport:
         self.data_generator = data_generator
         self.headers = ("position", "performance")
 
-    def create(self):
+    def create(self) -> list[tuple[str, str]]:
         """
         Create a sorted performance report from the data generator.
         """
-        pos_performances = defaultdict(list)
+        pos_performances: defaultdict[str, list[float]] = defaultdict(list)
 
         # Fill total performance per position
         for row in self.data_generator:
